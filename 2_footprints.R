f58e8ce04838dff5682119db8375d32c888e0cf8
hannahelander <51705758+hannahelander@users.noreply.github.com>
2019-07-12 16:05:55 +0200

    version control

---
+++ 2_footprints.R
@@ -0,0 +1,92 @@
+# -----------------------------------------
+# Footprints
+# -----------------------------------------
+# Calculate Footprints along the supply chain. 
+
+# prepare extension and define footprint
+
+e <- E$Biomass / X                    # Footprint: Biomass
+e[!is.finite(e)] <- 0
+
+MP <- e * L
+
+
+#########################################################
+# Biomass footprints along the Supply Chain - Status Quo 
+#########################################################
+
+# Total footprint
+FP_tot <- t(t(MP) * Y[,"DEU_Food"])           
+
+# per capita footprint
+population <- 82442336
+FP_capita <- sum(FP_tot) / population             # gives ~3 tonnes
+
+
+
+
+
+# For Plant-based products--------------------
+
+waste <- read.csv2(file = "data/waste_data_frame.csv")
+
+# create result matrix
+NrOfProducts <- 130
+#FP__chain <- data.frame(Scenario = rep("SQ", NrOfProducts),
+#                        products = rep(index$product, each = NrOfProducts),
+#                        product_group = rep(index$product_group, each = NrOfProducts))
+
+
+# Create a Y-matrix where all animal-products are 0 - Can we do it nicer!?
+Y_plant <- Y[,"DEU_Food"] 
+Y_plant[index$product_group %in% c("Livestock products", "Fish")] <- 0
+
+
+FP_plant <- t(t(MP) * Y_plant) ########  Error: cannot allocate vector of size 4.6 Gb
+# FP_plant <- sum(FP_plant)
+# FP_plant <- sum(colSums(MP) * Y_plant) ########  Error: cannot allocate vector of size 4.6 Gb
+
+
+# create output matrix
+supply_chain_FP <- data.frame(Scenario = rep("SQ", 4),
+                              chain_type = c("plant_based", "plant_based", "animal_based", "animal_based"),
+                              flow = c("waste", "cont", "waste", "cont"))
+
+
+# BIG CALCULATIONS--
+FP_tmp <- sum(FP_plant * waste$harvest_production) / 100               # waste shares are given as percentage (8% = 0,08)
+FP_plant_sum <- sum(FP_plant)
+
+supply_chain_FP$harvest_production <- c(FP_tmp, FP_plant_sum, NA, NA) # column has 4 rows # FAO production data reflect 'cont' flow
+
+supply_chain_FP$storage_transport <- c(FP_tmp, FP_plant_sum - FP_tmp, NA, NA) # column has 4 rows
+
+
+  
+### Create function!?  ####
+step.calculator <- function(waste_step, FP){                      # waste_step is column in waste, eg. waste$harvest_production?
+  FP_step <- c(sum(FP * waste_step),               
+               sum(FP - (FP * waste_step)), NA, NA)
+  FP_tmp <- FP  - (FP* waste_step)                  
+  return(list(FP_step,FP_tmp))
+}
+
+step_results <- listname[[1]]
+
+
+supply_chain_FP$storage_transport  <- step.calculator(waste$harvest_production, FP_tmp)[[1]]
+
+supply_chain_FP&processing         <- step.calculator(waste$processing)
+supply_chain_FP&distribution       <- step.calculator(waste$distribution)
+supply_chain_FP&final_consumption  <- step.calculator(waste$final_consumption)
+
+
+
+# For animal based products ----------------------
+Y_animal <- Y[,"DEU_Food"] 
+Y_animal[index$country=="DEU" & (!index$product_group=="Livestock products" 
+                                 | !index$product_group=="Milk" 
+                                 | !index$product_group=="Eggs" 
+                                 | !index$product_group=="Fish")] <- 0        # Set Y for all products that are not animal based
+
+
