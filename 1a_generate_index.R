345ea8ee5564f239bee2c35988c720c465085a99
hannahelander <51705758+hannahelander@users.noreply.github.com>
2019-07-15 17:41:21 +0200

    up to date

--- 1a_generate_index.R
+++ 1a_generate_index.R
@@ -4,4 +4,8 @@ NrOfCountries <- 192
 NrOfProducts <- 130
 index <- data.frame(country = rep(countries$ISO, each=NrOfProducts),
                     product = rep(products$Item, NrOfCountries),
+                    item_code = rep(products$Item.Code, NrOfCountries),
+                    com_group = rep(products$Com.Group, NrOfCountries),
                     product_group = rep(products$Group, NrOfCountries))
+
+write.csv2(waste, file = "data/index_data_frame.csv")     # write to file in data-folder! 
